e405ac8d22cfe9eefb52c89ca029c018adcdcc23
Dmitrii Rudik <dima.rudik@gmail.com>
2021-04-01 12:43:13 +0300

    To review

---
+++ src/RoadCamera.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RoadCamera {
+    public static void main(String[] args) throws IOException {
+        //Параметры программы
+        int maxOncomingSpeed = 40; // km/h
+        int speedFineGrade = 20; // km/h
+        int finePerGrade = 500; // RUB
+        int criminalSpeed = 180; // km/h
+        int fineMultiplier = 2;
+
+        //=============================================================
+
+        System.out.println("Введите скорость автомобиля:");
+
+        //Скорость автомобиля
+        int oncomingSpeed = Integer.parseInt((new BufferedReader(new InputStreamReader(System.in))).readLine());
+        if (oncomingSpeed >= criminalSpeed) {
+            System.out.println("Вызов полиции...");
+        } else if (oncomingSpeed > maxOncomingSpeed) {
+            int overSpeed = oncomingSpeed - maxOncomingSpeed;
+            int gradesCount = overSpeed / speedFineGrade;
+            int fine = finePerGrade * gradesCount * fineMultiplier;
+            System.out.println("Сумма штрафа: " + fine);
+        } else {
+            System.out.println("Скорость не превышена");
+        }
+    }
+}
