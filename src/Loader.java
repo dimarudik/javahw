f62440a80cba75e9eb3645ef402f7cc859f105f7
Dmitrii Rudik <dima.rudik@gmail.com>
2021-04-02 13:59:26 +0300

    3

--- src/Loader.java
+++ src/Loader.java
@@ -1,20 +1,37 @@
 
 public class Loader {
     public static void main(String[] args) {
-        int milkAmount = 200; // ml
-        int powderAmount = 5; // g
-        int eggsCount = 3; // items
+        int milkAmount = 600; // ml
+        int powderAmount = 800; // g
+        int eggsCount = 12; // items
         int sugarAmount = 5; // g
         int oilAmount = 30; // ml
         int appleCount = 8;
 
         //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
-        System.out.println("Pancakes");
+        if (powderAmount >= 400 && sugarAmount >= 10 && milkAmount >= 1000 && oilAmount >= 30) {
+            powderAmount -= 400;
+            sugarAmount -= 10;
+            milkAmount -= 1000;
+            oilAmount -= 30;
+            System.out.println("Pancakes");
+        }
 
         //milk - 300 ml, powder - 5 g, eggs - 5
-        System.out.println("Omelette");
+        if (milkAmount >= 300 && powderAmount >= 5 && eggsCount >= 5) {
+            milkAmount -= 300;
+            powderAmount -= 5;
+            eggsCount -= 5;
+            System.out.println("Omelette");
+        }
 
         //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
-        System.out.println("Apple pie");
+        if (appleCount >= 3 && milkAmount >= 100 && powderAmount >= 300 && eggsCount >= 4) {
+            appleCount -= 3;
+            milkAmount -= 100;
+            powderAmount -= 300;
+            eggsCount -= 4;
+            System.out.println("Apple pie");
+        }
     }
 }
